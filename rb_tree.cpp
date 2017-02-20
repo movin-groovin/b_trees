80681d8bcee1a900233cf5e7023f43721346368d
Yaroslav Neusypov <yaneusypov@2reallife.com>
2017-02-20 10:00:06 +0300

    firest files

---
+++ rb_tree.cpp
@@ -0,0 +1,32 @@
+
+// g++ -std=c++14 rb_tree.cpp -o rb_tree
+
+#include <iostream>
+#include <string>
+
+
+
+namespace algo
+{
+	//
+	;
+	//
+}
+
+
+
+int main (int argc, char ** argv)
+{
+	using namespace algo;
+	
+	;
+	
+	return 0;
+}
+
+
+
+
+
+
+
